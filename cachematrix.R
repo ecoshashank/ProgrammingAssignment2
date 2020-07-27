c3aef13056e1348ecb39db74e7769e953d40da01
ecoshashank <46953802+ecoshashank@users.noreply.github.com>
2020-07-27 16:11:36 +0530

    assignment lexical scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Thisfunction caches the Inverse of a matrix which helps to   
+## avoid recalculation when the inverse is requested for the same 
+##matrix again or calculate a new inverse if the matrix changes and caches the result in R's memory
 
-## Write a short comment describing this function
+## This function creates a  matrix and provides a function which set values,
+## get values,set inverse & get inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv<<-NULL
+  }
+  get <- function(){x}
+  setInverse <- function(inverse) {inv <<- inverse}
+  getInverse <- function() {inv}
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
+## The below function will will look into cache to see if inverse exist for matrix 
+## and then retrieve the value if its the same matrix however will calculate 
+##if no inverse exist or matrix is not same
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,....){
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
 }
